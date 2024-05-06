a445c71a7e4f20b40d1d85750248336c6a9d071c
Jason <jay.jpy.wong@gmail.com>
2024-05-07 00:20:27 +0200

    Added standard view transition
    Testing, need to build further

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -94,5 +94,17 @@ luisterBtn?.addEventListener('click', () => {
 });
 
 
+function spaNavigate(data) {
+    // Fallback for browsers that don't support this API:
+    if (!document.startViewTransition) {
+      updateTheDOMSomehow(data);
+      return;
+    }
+  
+    // With a transition:
+    document.startViewTransition(() => updateTheDOMSomehow(data));
+  }
+
+
 
 
